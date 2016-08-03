bfc7b9469f03944eb4c64cf1e026fb4663166b16
Anoop <anoopkrsonkar@gmail.com>
2016-08-03 13:39:00 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,22 @@
+<%=javascript_include_tag 'jquery-1.11.1','jquery.validate', :defer=>'defer' %><%=javascript_include_tag 'validation','bootstrap.min','bootstrap-select.min','fileinput.min','html5','search','jquery-ui', :defer=>'defer' %>
+
+https://github.com/anoopkrsonkar/testing.git
+kjhbsdcijdhbsodk
+
+
+
+
+<<<<<<< HEAD
+12
+
+
+=======
+11
+>>>>>>> master
+
+https://github.com/anoopkrsonkar/testing.git
+
+iuyt
+kjhedf
+
+a
